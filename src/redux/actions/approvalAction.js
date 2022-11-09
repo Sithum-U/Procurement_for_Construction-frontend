cd364b09f3079a869198ef9b06ebbc8719df7a97
Sithum-U <avsithum@gmail.com>
2022-11-09 17:32:07 +0530

    Updated approvalAction.js

--- src/redux/actions/approvalAction.js
+++ src/redux/actions/approvalAction.js
@@ -1,50 +1,50 @@
 import { ActionTypes } from '../constants';
 import axios from 'axios';
 import { toast } from "react-toastify";
-import {ToastObjects} from "../../util/toastObject";
+import { ToastObjects } from "../../util/toastObject";
 
 export const approval = () => () => {
 
-	toast.success("Approval Requested", ToastObjects);	
+  toast.success("Approval Requested", ToastObjects);
 }
 
 export const createApproval = (reqData) => async (dispatch, getState) => {
-    
-    try {
-      dispatch({ type: ORDER_CREATE_REQUEST });
 
-      const response = await axios.post(
-        `orders/`,
-        reqData
-      );
+  try {
+    dispatch({ type: ORDER_CREATE_REQUEST });
 
-      const responseData = response.data;
+    const response = await axios.post(
+      `orders/`,
+      reqData
+    );
 
-      if (!responseData.status) {
-        toast.error(responseData.message, ToastObjects);  
-      }else{
-        toast.success(responseData.message, ToastObjects);        
-        dispatch({ type: ORDER_CREATE_SUCCESS, payload: responseData });
-        dispatch({ type: CLEAR_CART_ITEM });
-        localStorage.removeItem('cartItems');
-      }
-      
-      
-    } catch (error) {
-      
-      const message =
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message;
-      if (message === "Not authorized, token failed") {
-        dispatch(logout());
-      }
+    const responseData = response.data;
 
-      toast.error(message, ToastObjects);
-
-      dispatch({
-        type: ORDER_CREATE_FAIL,
-        payload: message,
-      });
+    if (!responseData.status) {
+      toast.error(responseData.message, ToastObjects);
+    } else {
+      toast.success(responseData.message, ToastObjects);
+      dispatch({ type: ORDER_CREATE_SUCCESS, payload: responseData });
+      dispatch({ type: CLEAR_CART_ITEM });
+      localStorage.removeItem('cartItems');
     }
-  };
+
+
+  } catch (error) {
+
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
+
+    toast.error(message, ToastObjects);
+
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload: message,
+    });
+  }
+};
