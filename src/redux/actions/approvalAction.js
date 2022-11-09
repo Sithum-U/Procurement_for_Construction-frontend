ff58f1b1d52ad70bde4b675dbc7fb2eaea594e42
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2022-11-09 17:27:07 +0530

    is approved front end edited

--- src/redux/actions/approvalAction.js
+++ src/redux/actions/approvalAction.js
@@ -1,50 +1,42 @@
-import { ActionTypes } from '../constants';
-import axios from 'axios';
+import { ActionTypes } from "../constants";
+import axios from "axios";
 import { toast } from "react-toastify";
-import {ToastObjects} from "../../util/toastObject";
+import { ToastObjects } from "../../util/toastObject";
 
 export const approval = () => () => {
+  toast.success("Approval Requested", ToastObjects);
+};
 
-	toast.success("Approval Requested", ToastObjects);	
-}
+// export const createApproval = (reqData) => async (dispatch, getState) => {
+//   try {
+//     dispatch({ type: ORDER_CREATE_REQUEST });
 
-export const createApproval = (reqData) => async (dispatch, getState) => {
-    
-    try {
-      dispatch({ type: ORDER_CREATE_REQUEST });
+//     const response = await axios.post(`orders/`, reqData);
 
-      const response = await axios.post(
-        `orders/`,
-        reqData
-      );
+//     const responseData = response.data;
 
-      const responseData = response.data;
+//     if (!responseData.status) {
+//       toast.error(responseData.message, ToastObjects);
+//     } else {
+//       toast.success(responseData.message, ToastObjects);
+//       dispatch({ type: ORDER_CREATE_SUCCESS, payload: responseData });
+//       dispatch({ type: CLEAR_CART_ITEM });
+//       localStorage.removeItem("cartItems");
+//     }
+//   } catch (error) {
+//     const message =
+//       error.response && error.response.data.message
+//         ? error.response.data.message
+//         : error.message;
+//     if (message === "Not authorized, token failed") {
+//       dispatch(logout());
+//     }
 
-      if (!responseData.status) {
-        toast.error(responseData.message, ToastObjects);  
-      }else{
-        toast.success(responseData.message, ToastObjects);        
-        dispatch({ type: ORDER_CREATE_SUCCESS, payload: responseData });
-        dispatch({ type: CLEAR_CART_ITEM });
-        localStorage.removeItem('cartItems');
-      }
-      
-      
-    } catch (error) {
-      
-      const message =
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message;
-      if (message === "Not authorized, token failed") {
-        dispatch(logout());
-      }
+//     toast.error(message, ToastObjects);
 
-      toast.error(message, ToastObjects);
-
-      dispatch({
-        type: ORDER_CREATE_FAIL,
-        payload: message,
-      });
-    }
-  };
+//     dispatch({
+//       type: ORDER_CREATE_FAIL,
+//       payload: message,
+//     });
+//   }
+// };
--- src/redux/reducers/orderReducers.js
+++ src/redux/reducers/orderReducers.js
@@ -1,6 +1,7 @@
-import {OrderConstants} from "../constants";
-const { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL} = OrderConstants;
-const { APPROVAL_CREATE_REQUEST, APPROVAL_CREATE_SUCCESS, APPROVAL_CREATE_FAIL} = ApprovalConstants;
+import { OrderConstants } from "../constants";
+const { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL } =
+  OrderConstants;
+// const { APPROVAL_CREATE_REQUEST, APPROVAL_CREATE_SUCCESS, APPROVAL_CREATE_FAIL} = ApprovalConstants;
 
 // Order Create
 export const orderCreateReducer = (state = {}, action) => {
@@ -19,15 +20,15 @@ export const orderCreateReducer = (state = {}, action) => {
 };
 
 // Approval Create
-export const approvalCreateReducer = (state = {}, action) => {
-  switch (action.type) {
-    case APPROVAL_CREATE_REQUEST:
-      return { loading: true };
-    case APPROVAL_CREATE_SUCCESS:
-      return { loading: false, success: true, approval: action.payload };
-    case APPROVAL_CREATE_FAIL:
-      return { loading: false, error: action.payload };
-    default:
-      return state;
-  }
-};
+// export const approvalCreateReducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case APPROVAL_CREATE_REQUEST:
+//       return { loading: true };
+//     case APPROVAL_CREATE_SUCCESS:
+//       return { loading: false, success: true, approval: action.payload };
+//     case APPROVAL_CREATE_FAIL:
+//       return { loading: false, error: action.payload };
+//     default:
+//       return state;
+//   }
+// };
